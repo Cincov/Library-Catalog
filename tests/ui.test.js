5bb28a27087302a840e10a1a29e0eb573b48b194
cincov <you@example.com>
2024-06-09 09:25:00 +0300

    add login user test

--- tests/ui.test.js
+++ tests/ui.test.js
@@ -45,3 +45,26 @@ test('Verify Register link is content "Register"', async ({page}) => {
 
     expect(contentText).toEqual("Register"); 
 });
+
+test('Verify valid user can login', async ({page}) => {
+    await page.goto(appUrl);
+    await page.waitForSelector('nav.navbar');
+   
+    //Navigate to login page
+    const loginLink = await page.$('a[href="/login"]');
+    await loginLink.click();
+
+    //Fill the user data
+    await page.fill('#email', "peter@abv.bg");
+    await page.fill('#password', "123456");
+
+    //Click on login button
+    const loginBtn = await page.$('#login-form > fieldset > input');
+    await loginBtn.click();
+
+    //Verify the logout buttin is present
+    const logoutnBtn =  await page.$('#logoutBtn');
+    const contentText = await logoutnBtn.textContent();
+
+    expect(contentText).toEqual("Logout");
+});
